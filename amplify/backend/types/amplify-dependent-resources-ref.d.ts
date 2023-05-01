f26aa2c360040d7960f41ef2acd80defd8fff7f1
Hari Makesh <84830864+Harimakesh@users.noreply.github.com>
2023-05-01 11:12:16 +0530

    First Commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "chess": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
